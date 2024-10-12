d39651ce5f47be98641fd0932e59ce2c0d6254ac
Amanda Prates (Jubilly) <amandaprates-12@hotmail.com>
2024-10-12 19:36:24 -0300

    testing

--- iac/sg.tf
+++ iac/sg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "ecommerce_sg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecommere_allow_ssh" {
+resource "aws_vpc_security_group_ingress_rule" "ecommerce_sg_allow_ssh" {
   security_group_id = aws_security_group.ecommerce_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
@@ -16,7 +16,7 @@ resource "aws_vpc_security_group_ingress_rule" "ecommere_allow_ssh" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecommere_allow_http" {
+resource "aws_vpc_security_group_ingress_rule" "ecommerce_sg_allow_http" {
   security_group_id = aws_security_group.ecommerce_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
@@ -24,7 +24,7 @@ resource "aws_vpc_security_group_ingress_rule" "ecommere_allow_http" {
   to_port           = 80
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecommere_allow_api" {
+resource "aws_vpc_security_group_ingress_rule" "ecommerce_sg_allow_api" {
   security_group_id = aws_security_group.ecommerce_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 5000
@@ -32,7 +32,7 @@ resource "aws_vpc_security_group_ingress_rule" "ecommere_allow_api" {
   to_port           = 5000
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecommere_allow_api2" {
+resource "aws_vpc_security_group_ingress_rule" "ecommerce_sg_allow_api2" {
   security_group_id = aws_security_group.ecommerce_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 5001
